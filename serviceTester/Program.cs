23c952f2d664b21dc6e881488df2cd187de41f9f
Momo The Monster <surya@vrchat.com>
2022-08-18 16:55:48 -0700

    Adds abstract logger

---
+++ serviceTester/Program.cs
@@ -0,0 +1,38 @@
+﻿using Makaretu.Dns;
+using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using VRC.OSCQuery;
+
+namespace Browser
+{
+    class Program
+    {
+        static ILogger _programLogger;
+        
+        public static void Main(string[] args)
+        {
+            var r = new Random();
+            var name = Guid.NewGuid().ToString();
+            var httpPort = r.Next(5000, 9999);
+            var oscPort = r.Next(5000, 9999);
+
+            var loggerFactory = LoggerFactory.Create(config => config.AddConsole());
+            _programLogger = loggerFactory.CreateLogger<Program>();
+            _programLogger.LogInformation($"Started {name} at http: {httpPort} osc: {oscPort}");
+
+            var libLogger = loggerFactory.CreateLogger<OSCQueryService>();
+            var q = new OSCQueryService(name, httpPort, oscPort, libLogger );
+            q.OnProfileAdded += OnProfileAdded;
+            
+            Console.ReadKey();
+            q.Dispose();
+        }
+
+        private static void OnProfileAdded(ServiceProfile obj)
+        {
+            var srv = obj.Resources.OfType<SRVRecord>().First();
+            _programLogger.LogInformation($"Found {obj.FullyQualifiedName} on {srv.Port}");
+        }
+    }
+}
--- vrc-oscquery-lib/OSCQueryService.cs
+++ vrc-oscquery-lib/OSCQueryService.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Makaretu.Dns;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json.Linq;
 
 namespace VRC.OSCQuery
 {
     public class OSCQueryService : IDisposable
     {
+
+        private readonly ILogger _logger;
         private const int DefaultPortHttp = 8080;
         private const int DefaultPortOsc = 9000;
         private const string DefaultServerName = "OSCQueryService";
@@ -30,15 +33,25 @@ namespace VRC.OSCQuery
         private HashSet<ServiceProfile> _oscQueryServices = new();
         private HashSet<ServiceProfile> _oscServices = new();
 
-        // Track getters and setters to respond to incoming value queries / updates
+        // Track getters incoming value queries
         private Dictionary<string, Func<dynamic>> _getters = new();
         private Dictionary<string, JObject> _oscPaths = new();
 
         private string _localOscUdpServiceName = $"{Attributes.SERVICE_OSC_UDP}.local";
         private string _localOscJsonServiceName = $"{Attributes.SERVICE_OSCJSON_TCP}.local";
 
-        public OSCQueryService(string serverName = DefaultServerName, int httpPort = DefaultPortHttp, int oscPort = DefaultPortOsc)
+        private readonly HashSet<string> _matchedNames;
+
+        public OSCQueryService(string serverName = DefaultServerName, int httpPort = DefaultPortHttp, int oscPort = DefaultPortOsc, ILogger? logger = null)
         {
+            // Construct hashset for services to track
+            _matchedNames = new HashSet<string>() { 
+                _localOscUdpServiceName, _localOscJsonServiceName
+            };
+            
+            // Set up logging
+            _logger = logger ?? NullLogger.Instance;
+            
             try
             {
                 this.serverName = serverName;
@@ -66,7 +79,7 @@ namespace VRC.OSCQuery
                     _mdns.SendQuery(_localOscJsonServiceName);
                 };
                 
-                _mdns.AnswerReceived += MdnsOnAnswerReceived;
+                _mdns.AnswerReceived += OnRemoteServiceInfo;
 
                 _mdns.Start();
                 
@@ -74,33 +87,26 @@ namespace VRC.OSCQuery
                 _listener.Prefixes.Add($"http://localhost:{httpPort}/");
                 _listener.Start();
 
-                // RefreshOtherServices();
-                
                 Task.Run(() => HttpListenerLoop());
                 _dorunrun = true;
             }
             catch (Exception e)
             {
                 _dorunrun = false;
-                Console.Error.Write(e.Message);
-                // throw;
+                _logger.LogError($"Could not start OSCQuery service: {e.Message}");
             }
 
             BuildRootResponse();
         }
 
-        private void MdnsOnAnswerReceived(object? sender, MessageEventArgs eventArgs)
+        private void OnRemoteServiceInfo(object? sender, MessageEventArgs eventArgs)
         {
-            var matchedNames = new HashSet<string>()
-            {
-                _localOscUdpServiceName, _localOscJsonServiceName
-            };
-
             var response = eventArgs.Message;
             
             try
             {
-                var name = response.Answers.First(r => matchedNames.Contains(r?.CanonicalName)).CanonicalName;
+                // Doing lots of LINQ-y stuff here and catching exceptions below for unmatched services. Open to other ideas!
+                var name = response.Answers.First(r => _matchedNames.Contains(r?.CanonicalName)).CanonicalName;
                 var srvRecord = response.AdditionalRecords.OfType<SRVRecord>().First();
                 var port = srvRecord.Port;
                 var domainName = srvRecord.Name.Labels;
@@ -109,17 +115,32 @@ namespace VRC.OSCQuery
                 var ips = response.AdditionalRecords.OfType<ARecord>().Select(r => r.Address);
                 var profile = new ServiceProfile(instanceName, serviceName, srvRecord.Port, ips);
 
-                if (!_oscServices.Any(p => p.FullyQualifiedName == profile.FullyQualifiedName))
+                if (name.CompareTo(_localOscUdpServiceName) == 0)
                 {
-                    _oscServices.Add(profile);
-                    OnProfileAdded?.Invoke(profile);
-                    Trace.WriteLine($"Found match {name} on port {port}");
+                    if (!_oscServices.Any(p => p.FullyQualifiedName == profile.FullyQualifiedName))
+                    {
+                        _oscServices.Add(profile);
+                        OnProfileAdded?.Invoke(profile);
+                        _logger.LogInformation($"Found match {name} on port {port}");
+                    }
                 }
-                
+                else if (name.CompareTo(_localOscJsonServiceName) == 0)
+                {
+                    if (!_oscQueryServices.Any(p => p.FullyQualifiedName == profile.FullyQualifiedName))
+                    {
+                        _oscQueryServices.Add(profile);
+                        OnProfileAdded?.Invoke(profile);
+                        _logger.LogInformation($"Found match {name} on port {port}");
+                    }
+                }
+                else
+                {
+                    
+                }
             }
             catch (Exception e)
             {
-                Console.Error.WriteLine($"Exception parsing answer for {response.Answers.First().Name}: {e.Message}");
+                _logger.LogError($"Exception parsing answer from {eventArgs.RemoteEndPoint}: {e.Message}");
             }
         }
 
@@ -154,7 +175,7 @@ namespace VRC.OSCQuery
                     }
                     catch (Exception e)
                     {
-                        Console.Error.WriteLine(e);
+                        _logger.LogError($"Could not construct and send Host Info: {e.Message}");
                     }
                 }
                 else
@@ -199,7 +220,7 @@ namespace VRC.OSCQuery
             var oscType = Attributes.OSCTypeFor(typeof(T));
             if (string.IsNullOrWhiteSpace(oscType))
             {
-                Console.Error.WriteLine($"Could not add {name} to OSCQueryService because type {typeof(T)} is not supported.");
+                _logger.LogError($"Could not add {name} to OSCQueryService because type {typeof(T)} is not supported.");
                 return;
             }
 
