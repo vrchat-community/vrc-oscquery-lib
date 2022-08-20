790318fdb78cc2141ac40fd4745c48d688364162
Momo The Monster <surya@vrchat.com>
2022-08-19 19:07:51 -0700

    Adds DataReceiver with working int receivers

--- Examples/DataReceiver/FindServiceDialog.cs
+++ Examples/DataReceiver/FindServiceDialog.cs
@@ -1,7 +1,73 @@
-﻿namespace VRC.OSCQuery.Examples
+﻿using System.Collections.Generic;
+using System.Linq;
+using Makaretu.Dns;
+using Terminal.Gui;
+
+namespace VRC.OSCQuery.Examples.DataReceiver
 {
-    public class FindServiceDialog
+    public class FindServiceDialog : Dialog
     {
-        
+        private TextField oscItemField;
+        private ListView _listView;
+        private ServiceProfile _selectedProfile;
+
+        private OSCQueryService _service;
+
+        public FindServiceDialog(OSCQueryService service)
+        {
+            _service = new OSCQueryService( OSCQueryService.DefaultServerName + "1", OSCQueryService.DefaultPortHttp + 10, OSCQueryService.DefaultPortOsc + 10);
+            
+            Width = 45;
+            Height = 10;
+            X = Pos.Center();
+            Y = Pos.Center();
+
+            var connectButton = new Button("Connect", true);
+            connectButton.Enabled = false;
+            connectButton.Clicked += () =>
+            {
+                if (_selectedProfile != null)
+                {
+                    Application.Top.Add(new DataReceiver.ListServiceData(_selectedProfile));
+                    Application.Top.Remove(this);
+                }
+            };
+            AddButton(connectButton);
+
+            _listView = new ListView()
+            {
+                X = 1,
+                Y = 1,
+                Width = Dim.Fill(2),
+                Height = Dim.Fill(2),
+            };
+            _listView.SelectedItemChanged += args =>
+            {
+                _selectedProfile = _oscqServices[args.Item];
+                connectButton.Enabled = true;
+            };
+            Add(_listView);
+
+            _service.OnProfileAdded += _ =>
+            {
+                RefreshListings();
+            };
+
+            Enter += _ => RefreshListings();
+        }
+
+        private List<ServiceProfile> _oscqServices;
+
+        public void RefreshListings()
+        {
+            _oscqServices = _service.GetOSCQueryServices().ToList();
+            var foundServices = _oscqServices.Count > 0;
+            Title = foundServices ? $"OSCQuery Services Found: {_oscqServices.Count}" : "Searching for OSCQuery Services...";
+            
+            _listView.Visible = foundServices;
+            _listView.SetSource(_oscqServices.Select(service=>service.InstanceName.ToString()).ToList());
+
+            SetNeedsDisplay();
+        }
     }
 }
---
+++ Examples/DataReceiver/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Makaretu.Dns;
+using Newtonsoft.Json;
+using Terminal.Gui;
+
+namespace VRC.OSCQuery.Examples.DataReceiver
+{
+    class DataReceiver
+    {
+
+        private static OSCQueryService _service;
+        
+        static void Main ()
+        {
+            Application.Init ();
+            
+            // Add both menu and win in a single call
+            Application.Top.Add (new FindServiceDialog(_service));
+            
+            Application.Run ();
+            Application.Shutdown ();
+        }
+
+        public class ListServiceData : Window
+        {
+            private int _tcpPort;
+            private TextView _textView;
+            private ServiceProfile _profile;
+            private SRVRecord _srvRecord;
+            
+            public ListServiceData(ServiceProfile profile)
+            {
+                _profile = profile;
+                _srvRecord = profile.Resources.OfType<SRVRecord>().First();
+                _tcpPort = _srvRecord.Port;
+
+                _textView = new TextView()
+                {
+                    Width = Dim.Fill(2),
+                    Height = Dim.Fill(2)
+                };
+
+                Title = $"{_profile.InstanceName} on {_srvRecord.Port}";
+                
+                Add(_textView);
+                RefreshData();
+            }
+
+            private async Task RefreshData()
+            {
+                var response = await new HttpClient().GetAsync($"http://localhost:{_tcpPort}/");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return;
+                }
+                
+                var responseString = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<OSCQueryRootNode>(responseString);
+
+                var sb = new StringBuilder();
+                foreach (var pair in result.Contents)
+                {
+                    sb.AppendLine($"{pair.Key}: {pair.Value.Value}");
+                }
+
+                _textView.Text = sb.ToString();
+                await Task.Delay(500); // poll every half second
+                RefreshData();
+            } 
+        }
+    }
+}
