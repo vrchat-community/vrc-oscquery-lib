f2b43d2de4049bb3f05a67ae6f017380c59f160d
Momo The Monster <momodmonster@gmail.com>
2022-08-25 14:36:30 -0700

    Adds very simple logger to examples

---
+++ Examples/DataSender/OSCQueryServiceWindow.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Terminal.Gui;
+
+namespace VRC.OSCQuery.Examples
+{
+    class OSCQueryServiceWindow : Window
+        {
+            private OSCQueryService _service;
+            private TextField oscItemField;
+
+            public OSCQueryServiceWindow(string name, int tcpPort, int oscPort, ILogger logger)
+            {
+                _service = new OSCQueryService(name, tcpPort, oscPort, logger);
+                Title = $"{name} TCP: {tcpPort} OSC: {oscPort}";
+                Add(MakeIntParams(10));
+            }
+
+            public View MakeIntParams(int count)
+            {
+                var result = new FrameView("Params and Values - Press to Randomize")
+                {
+                    X = 1,
+                    Y = 2,
+                    Height = Dim.Fill(2),
+                    Width = Dim.Fill(2),
+                };
+
+                var r = new Random();
+                var wordSet = new Bogus.DataSets.Hacker();
+                var GenerateIntProp = new Func<string, int, string>((propertyName, intValue) => $"/{propertyName} {intValue}");
+                _intParams = new int[count];
+                for (int i = 0; i < count; i++)
+                {
+                    var name = $"{wordSet.Adjective()}-{wordSet.Noun()}";
+                    
+                    var newValue = r.Next(0, 99);
+                    SetIntParam(i, newValue);
+                    
+                    var b = new Button(GenerateIntProp(name, newValue))
+                    {
+                        Width = Dim.Fill(),
+                        Height = 1,
+                        X = 1,
+                        Y = 1 + i,
+                        AutoSize = false,
+                        TextAlignment = TextAlignment.Left
+                    };
+                    var localIndex = i;
+                    b.Clicked += () =>
+                    {
+                        int value = r.Next(0, 99);
+                        SetIntParam(localIndex, value);
+                        b.Text = GenerateIntProp(name, value);
+                    };
+                    result.Add(b);
+                    _service.AddEndpoint<int>($"/{name}", Attributes.AccessValues.ReadOnly,  () => GetIntParam(localIndex).ToString());
+                }
+
+                return result;
+            }
+
+            public int GetIntParam(int i)
+            {
+                if (_intParams.Length > i)
+                {
+                    return _intParams[i];
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            public void SetIntParam(int i, int value)
+            {
+                if (_intParams.Length > i)
+                {
+                    _intParams[i] = value;
+                }
+            }
+
+            private int[] _intParams;
+        }
+}
