0a00e0b726b593855410c45e161c5a8296b1604c
Momo The Monster <momodmonster@gmail.com>
2022-10-17 15:08:23 -0700

    Fixes import of UnityMSLogger

--- Examples/OSCQueryExplorer-Unity/Packages/com.vrchat.oscquery/Samples/Chatbox/ChatboxCanvas.cs
+++ Examples/OSCQueryExplorer-Unity/Packages/com.vrchat.oscquery/Samples/Chatbox/ChatboxCanvas.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
-using VRC.OSCQuery.Samples.Shared;
+using VRC.OSCQuery.Examples.OSCQueryExplorerUnity;
 
 #pragma warning disable 4014
 
