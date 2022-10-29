e967e387b44722a95313e2fc118d8bef4263a6d2
Momo The Monster <momodmonster@gmail.com>
2022-10-28 20:34:05 -0700

    Adds Tracking Demo

--- Examples/OSCQueryExplorer-Unity/Packages/com.vrchat.oscquery/Samples/Shared/SceneChanger.cs
+++ Examples/OSCQueryExplorer-Unity/Packages/com.vrchat.oscquery/Samples/Shared/SceneChanger.cs
@@ -6,16 +6,19 @@ public class SceneChanger : MonoBehaviour
 {
     public Button ButtonChatbox;
     public Button ButtonReceiver;
+    public Button ButtonTrackers;
 
     private void Start()
     {
         ButtonChatbox.onClick.AddListener(()=>SceneManager.LoadScene(1));
         ButtonReceiver.onClick.AddListener(()=>SceneManager.LoadScene(2));
+        ButtonTrackers.onClick.AddListener(()=>SceneManager.LoadScene(3));
     }
 
     private void OnDestroy()
     {
         ButtonChatbox.onClick.RemoveAllListeners();
         ButtonReceiver.onClick.RemoveAllListeners();
+        ButtonTrackers.onClick.RemoveAllListeners();
     }
 }
